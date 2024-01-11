28e4a49bbe539b52572407b3edc0409a3f0a64a3
Yiang <yiangforwork@gmail.com>
2024-01-11 08:22:37 -0500

    Able to resize crop

--- app.js
+++ app.js
@@ -18,10 +18,15 @@ const dCropArea = document.querySelector(".crop-area");
 
 const cropInfo = {
     moveStart: false,
+    resize: false,
+    resizeW: false,
+    resizeH: false,
 
     areaOldX: 0,
     areaOldY: 0,
 
+    oldAreaW: 100,
+    oldAreaH: 100,
     areaW: 100,
     areaH: 100,
 
@@ -49,24 +54,56 @@ dImageCopper.addEventListener("mousedown", e => {
 
         cropInfo.areaOldX = cropInfo.areaX;
         cropInfo.areaOldY = cropInfo.areaY;
+
+        return;
+    }
+
+    const { target } = e;
+
+    if (target.classList.contains("crop-handle")) {
+        cropInfo.resize = true;
+
+        cropInfo.oldAreaH = cropInfo.areaH;
+        cropInfo.oldAreaW = cropInfo.areaW;
+
+        if (target.classList.contains("handle-left") || target.classList.contains("handle-right")) {
+            cropInfo.resizeW = true;
+        }
+
+        if (target.classList.contains("handle-top") || target.classList.contains("handle-bottom")) {
+            cropInfo.resizeH = true;
+        }
     }
 });
 
 dImageCopper.addEventListener("mousemove", e => {
-    if (cropInfo.moveStart) {
+    if (cropInfo.moveStart || cropInfo.resize) {
         cropInfo.currentX = e.offsetX;
         cropInfo.currentY = e.offsetY;
 
         const dx = cropInfo.currentX - cropInfo.startX;
         const dy = cropInfo.currentY - cropInfo.startY;
 
-        cropInfo.areaX += dx;
-        cropInfo.areaY += dy;
-        // cropInfo.areaX = cropInfo.areaOldX + dx;
-        // cropInfo.areaY = cropInfo.areaOldY + dy;
+        if (cropInfo.moveStart) {
+            cropInfo.areaX += dx;
+            cropInfo.areaY += dy;
+            // cropInfo.areaX = cropInfo.areaOldX + dx;
+            // cropInfo.areaY = cropInfo.areaOldY + dy;
+            updateArea();
+        }
 
-        updateArea();
-        console.log(dx, dy);
+        if (cropInfo.resize) {
+
+            if (cropInfo.resizeW) {
+                cropInfo.areaW = cropInfo.oldAreaW + dx;
+            }
+
+            if (cropInfo.resizeH) {
+                cropInfo.areaH = cropInfo.oldAreaH + dy;
+            }
+
+            updateArea();
+        }
     }
 });
 
@@ -75,13 +112,20 @@ dImageCopper.addEventListener("mouseup", e => {
         // cropInfo.areaX = cropInfo
         cropInfo.moveStart = false;
     }
+
+    cropInfo.resize = false;
+    cropInfo.resizeW = false;
+    cropInfo.resizeH = false;
+
+    cropInfo.oldAreaH = cropInfo.areaH;
+    cropInfo.oldAreaW = cropInfo.areaW;
 });
 
 const dCropButton = document.querySelector("#crop-btn");
 const dCropImg = document.querySelector(".img-el");
 const dCropImgWarp = document.querySelector(".image-wrap");
 
-function loadImage( src ) {
+function loadImage(src) {
     return new Promise(resolve => {
         const image = new Image();
         image.addEventListener("load", () => resolve(image));
@@ -92,7 +136,7 @@ function loadImage( src ) {
 
 loadImage("./sample.png").then(image => {
     cropInfo.image = image;
-    dCropImgWarp.prepend( image );
+    dCropImgWarp.prepend(image);
 });
 
 
@@ -105,21 +149,21 @@ function crop() {
     canvas.width = cropInfo.areaW;
     canvas.height = cropInfo.areaH;
 
-    ctx.drawImage( cropInfo.image, cropInfo.areaX, cropInfo.areaY, cropInfo.areaW, cropInfo.areaH, 0, 0, cropInfo.areaW, cropInfo.areaH );
+    ctx.drawImage(cropInfo.image, cropInfo.areaX, cropInfo.areaY, cropInfo.areaW, cropInfo.areaH, 0, 0, cropInfo.areaW, cropInfo.areaH);
 
     canvas.toBlob(blob => {
-        const imageData = new ClipboardItem({"image/png": blob});
+        const imageData = new ClipboardItem({ "image/png": blob });
         navigator.clipboard.write([imageData]);
     });
 }
 
 window.addEventListener("keydown", e => {
-    if( e.key === "-" && e.ctrlKey ) {
+    if (e.key === "-" && e.ctrlKey) {
         e.preventDefault();
         console.log(("CTRL+-"));
     }
 
-    if( e.key === "=" && e.ctrlKey ) {
+    if (e.key === "=" && e.ctrlKey) {
         e.preventDefault();
         console.log(("CTRL++"));
     }
