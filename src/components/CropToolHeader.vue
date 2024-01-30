3ff814f42b363ebc06e45ffa579d05aa4e9045d5
Yiang <yiangforwork@gmail.com>
2024-01-30 16:26:57 +0800

    update: add crop header tool

---
+++ src/components/CropToolHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <label for="crop-w">width</label>
+    <input type="number" v-model="cropStore.rect.w">
+
+    <label for="crop-w">height</label>
+    <input type="number" v-model="cropStore.rect.h">
+
+    <label for="crop-w">x</label>
+    <input type="number" v-model="cropStore.rect.x">
+
+    <label for="crop-w">y</label>
+    <input type="number" v-model="cropStore.rect.y">
+</template>
+
+<script setup lang="ts">
+import { useCropStore } from '@/stores/CropStore';
+
+const cropStore = useCropStore();
+</script>
--- src/pages/HomePage/Header.vue
+++ src/pages/HomePage/Header.vue
@@ -1,8 +1,9 @@
 <template>
     <header class="app-header">
-        <p>Header Here</p>
+        <CropToolHeader></CropToolHeader>
     </header>
 </template>
 
 <script setup lang="ts">
+import CropToolHeader from '@/components/CropToolHeader.vue';
 </script>
